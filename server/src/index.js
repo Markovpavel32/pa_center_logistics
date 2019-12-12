7e69b12f9c4b8684503401079fc442661e42740d
Pavel Markov <markovpavel32@gmail.com>
2019-12-12 22:12:23 +0300

    initial commit

--- server/src/index.js
+++ server/src/index.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const config = require('../config/config')
-const { Client } = require('pg')
-const sequelize_sync = require('../models/user')
+const sequelize_sync = require('./models/user')
 
 const app = express()
 app.use(morgan('combined'))
@@ -22,26 +21,5 @@ app.get('/posts', (req, res) => {
         }]
     )
 })
-
-const client = new Client({
-    host: '94.228.196.246',
-    port: 5434,
-    user: 'lk',
-    password: 'AJFGrLs6azpwE3k',
-    database: 'service'
-})
-
-client.connect(err => {
-    if (err) {
-        console.error('connection error', err.stack)
-    } else {
-        console.log('connected')
-    }
-})
-
-client.query(`SELECT site.receptiondocuments('{"customer_id" : "     YSEL" , "start_date" : "2019-09-01" , "stop_date" : "2019-12-01", "maximum_rows" : 600}');`)
-    .then(result => console.log(result.rows[0].receptiondocuments.doc_lines.length))
-    .catch(e => console.error(e.stack))
-    .then(() => client.end())
 
 sequelize_sync()

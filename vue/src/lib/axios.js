be1116fbc72e4ff1ec73a78eefdb4068b4e5696a
Pavel Markov <markovpavel32@gmail.com>
2020-01-13 14:34:17 +0300

    initial frontend

---
+++ vue/src/lib/axios.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import qs from 'qs'
+
+const port = 8081
+
+const axios_tuned = axios.create({
+  baseURL: location.protocol + '//' + location.hostname + ':' + port,
+  headers: {
+    'Content-type': 'application/x-www-form-urlencoded'
+  }
+})
+
+export const axios_post = (url, data, config) => {
+  const new_data = {}
+  for (let key in data) {
+    new_data[key] = data[key]
+  }
+  data = qs.stringify({ ...(new_data || {}) })
+  return axios_tuned.post(url, data, config)
+}
---
+++ vue/src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <span class="row">Личный кабинет клиента</span>
+    <span class="row font-weight-bold">Авторизация</span>
+    <span class="row">Для входа используйте предоставленный логин и пароль</span>
+    <input v-model="login" class="row"/>
+    <input v-model="password" class="row"/>
+    <button class="btn btn-danger d-block row" @click="auth()">Войти в кабинет</button>
+    <button class="btn btn-secondary d-block row">Получить логин и пароль</button>
+  </div>
+</template>
+
+<script>
+import { axios_post } from '../lib/axios'
+
+export default {
+  name: 'login',
+
+  data () {
+    return {
+      login: '',
+      password: ''
+    }
+  },
+
+  computed: {
+    model () {
+      return {
+        login: this.login,
+        password: this.password
+      }
+    }
+  },
+
+  methods: {
+    auth () {
+      axios_post('/login', this.model)
+        .then(() => { this.$router.push({ name: 'home' }) })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
